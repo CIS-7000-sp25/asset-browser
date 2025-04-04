ace33da547ac2275cfd2799f89e848a052415979
terskayl <97208428+terskayl@users.noreply.github.com>
2025-04-04 11:58:01 -0400

    Update settings.py

--- backend/asset_library/settings.py
+++ backend/asset_library/settings.py
@@ -161,3 +161,5 @@ else:
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+ALLOWED_HOSTS = ["usd-asset-library.up.railway.app", "127.0.0.1"]
