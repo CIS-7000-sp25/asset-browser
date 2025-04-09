942da65dade0e6c93eb8887cb17164711bc41b4a
Charles Wang <zwcharl@gmail.com>
2025-04-08 20:03:52 -0400

    rm/downgrade new packages, let upgrade tool handle it

--- astro.config.ts
+++ astro.config.ts
@@ -2,14 +2,9 @@ import react from "@astrojs/react";
 import vercel from "@astrojs/vercel";
 import { defineConfig } from "astro/config";
 
-import tailwindcss from "@tailwindcss/vite";
-
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
   integrations: [react()],
-  vite: {
-    plugins: [tailwindcss()],
-  },
 });
